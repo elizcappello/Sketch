0aed1ccb164315eddce66bda1395135425f56de3
Elizabeth Cappello <lizc@elizabeellosmbp.goucher.edu>
2016-07-23 09:17:40 -0400

    first commit
    First project

---
+++ sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(560, 340)
+  background (225,235,235)
+}
+
+function draw() {
+  
+  strokeWeight(4)
+  line (270,210,270,330)
+  strokeWeight(4)
+  line (270,20,270,120)
+  strokeWeight(4)
+  line (90,170,220,170)
+  strokeWeight(4)
+  line (320,170,450,170)
+  
+  fill (0,51,51)
+  stroke (0,51,51)
+  strokeWeight (1)
+  ellipse (270, 170, 100, 100)
+  fill (0,179,179)
+  stroke (0,51,51)
+  strokeWeight (1)
+  ellipse (270, 170, 50, 50)
+  fill (204,255,255)
+  stroke (0,51,51)
+  strokeWeight (1)
+  ellipse (270, 170, 30, 30)
+  
+  //fill (204,255,255);
+  //stroke (0,51,51)
+  //strokeWeight (2)
+  //triangle (135,160,280,80,280,120)
+  
+}
